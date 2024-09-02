ad2bbe3374e0d89cfcb544d89174bbd9054ae322
msantana0612 <marcos.santana0612@gmail.com>
2024-09-02 12:11:41 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Cargar el notebook por defecto al iniciar la página
+    loadNotebook('notebook1.html');
+});
+
+function loadNotebook(notebookFile) {
+    const iframe = document.getElementById('notebookViewer');
+    iframe.src = notebookFile;
+}
