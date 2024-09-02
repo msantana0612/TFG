b98c03f59fc92d3ffc56301e1f42e386943bc4e0
msantana0612 <marcos.santana0612@gmail.com>
2024-09-02 12:59:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 function loadNotebook(notebookFile) {
     const iframe = document.getElementById('notebookViewer');
-    iframe.src = notebookFile;
+    iframe.src = '/TFG/html_outputs/notebooks/' + notebookFile;
 }
