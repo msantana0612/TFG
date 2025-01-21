bca2d6638be5e28d8cfc406263503a275f4a66f1
msantana0612 <marcos.santana0612@gmail.com>
2025-01-21 08:11:36 +0100

    Test result path correction

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,16 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest', // Usa el preset correcto para TypeScript
-  testEnvironment: 'jsdom', // Define el entorno de pruebas para un DOM simulado
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: './test-results/jest', outputName: 'results.xml' }]
+  ]
 };
-
-export const reporters = [
-  'default',
-  ['jest-junit', { outputDirectory: './test-results/jest', outputName: 'results.xml' }]
-];
 
 export default config;
